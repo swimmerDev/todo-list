980e9d9c0b9e746e596a887de0c5db5f78fc0fde
swimmer <lev23599@gmail.com>
2023-02-11 00:46:55 +0700

    del theme context

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,10 @@ import "./styles/App.css";
 import MainTitle from './component/UI/MainTitle/MainTitle';
 import TaskTitle from './component/UI/TaskTitile/TaskTitle';
 import AddLine from './component/AddLine';
-import { ThemeContextType } from './context/ThemeContext';
 import { TasksContextProvider } from './context/TaskContext';
 import TaskList from './component/TaskList';
 
 function App() {
-  const [theme, setTheme] = useState<ThemeContextType>("light");
-
   return (
     <div className="App">
       <MainTitle > Task Manager </MainTitle>
--- src/context/ThemeContext.ts
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react";
-
-export type ThemeContextType = "light" | "dark";
-
-export const ThemeContext = createContext<ThemeContextType>("light");
